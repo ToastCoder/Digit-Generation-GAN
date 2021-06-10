efc4fa05a8bb6398601f9ef7272713ba2ed96802
ToastCoder <vicky.pcbasic@gmail.com>
2021-06-10 17:23:36 +0530

    Some changes

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# IMPORTING REQUIRED LIBRARIES
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import os
+import tensorflow as tf
+
+# GETTING DATA
+data = tf.keras.datasets.mnist
+(x_train,y_train),(x_val,y_val) = data.load_data()
+
+x_train,x_val = x_train/255.0*2-1,x_val/255.0*2-1
 
