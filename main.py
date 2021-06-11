57e34edb5bdbd8b1f2045c08f5242235ffc3439d
ToastCoder <vicky.pcbasic@gmail.com>
2021-06-11 10:54:35 +0530

    Some changes

--- main.py
+++ main.py
@@ -78,7 +78,6 @@ def sample_images(epoch):
   noise = np.random.randn(rows * cols, latent_dim)
   imgs = generator.predict(noise)
 
-  # Rescale images 0 - 1
   imgs = 0.5 * imgs + 0.5
 
   fig, axs = plt.subplots(rows, cols)
@@ -91,19 +90,15 @@ def sample_images(epoch):
   fig.savefig("gan_images/%d.png" % epoch)
   plt.close()
 
-# Main training loop
+
 for epoch in range(EPOCHS):
 
-  # Select a random batch of images
   idx = np.random.randint(0, x_train.shape[0], BATCH_SIZE)
   real_imgs = x_train[idx]
   
-  # Generate fake images
   noise = np.random.randn(BATCH_SIZE, latent_dim)
   fake_imgs = generator.predict(noise)
-  
-  # Train the discriminator
-  # both loss and accuracy are returned
+
   d_loss_real, d_acc_real = discriminator.train_on_batch(real_imgs, ones)
   d_loss_fake, d_acc_fake = discriminator.train_on_batch(fake_imgs, zeros)
   d_loss = 0.5 * (d_loss_real + d_loss_fake)
@@ -114,11 +109,11 @@ for epoch in range(EPOCHS):
   noise = np.random.randn(BATCH_SIZE, latent_dim)
   g_loss = combined_model.train_on_batch(noise, ones)
   
-  # do it again!
+
   noise = np.random.randn(BATCH_SIZE, latent_dim)
   g_loss = combined_model.train_on_batch(noise, ones)
   
-  # Save the losses
+
   d_losses.append(d_loss)
   g_losses.append(g_loss)
   
